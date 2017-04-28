a467bf1b4ad7b7893bc9d32b27b7d5eb8a86a531
u0771753 <u0771753@kcmac-73.scl.utah.edu>
2017-04-27 19:03:19 -0600

    Started on bestiary logic

---
+++ DnDHelper/BestiaryCollectionController.swift
@@ -0,0 +1,9 @@
+//
+//  BestiaryCollectionController.swift
+//  DnDHelper
+//
+//  Created by u0771753 on 4/27/17.
+//  Copyright © 2017 u0771753. All rights reserved.
+//
+
+import Foundation
---
+++ DnDHelper/BestiaryEntryCell.swift
@@ -0,0 +1,9 @@
+//
+//  BestiaryEntryCell.swift
+//  DnDHelper
+//
+//  Created by u0771753 on 4/27/17.
+//  Copyright © 2017 u0771753. All rights reserved.
+//
+
+import Foundation
